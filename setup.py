d9ff96773f5e47331592b39c0ef20f584591a73a
MarcoTuc <marco.tuccio95@gmail.com>
2024-10-15 15:39:33 +0200

    sharing+

--- setup.py
+++ setup.py
@@ -3,15 +3,12 @@
 from setuptools import setup, find_packages
 import os
 
-home_dir = os.path.expanduser("~")
-work_dir = os.path.join(home_dir, "synth")
+work_dir = os.getcwd()
 env_dir = os.path.join(work_dir, "menv")
 
 # make necessary directories
-try: os.makedirs(os.path.join(work_dir, "models"), exist_ok=False)
-except OSError: pass 
-try: os.makedirs(os.path.join(work_dir, "data"), exist_ok=False)
-except OSError: pass
+os.makedirs(os.path.join(work_dir, "models"), exist_ok=False)
+os.makedirs(os.path.join(work_dir, "data"), exist_ok=False)
 
 # make directories for highlighting folder -- will be removed once xent package is complete
 hili_dir = os.path.join(work_dir, "highlighting")
@@ -20,8 +17,16 @@ except OSError: pass
 try: os.makedirs(os.path.join(hili_dir, "data"), exist_ok=False)
 except OSError: pass
 
+requirements = []
+with open('requirements.txt', 'r') as file:
+    for line in file:
+        requirement = line.strip()
+        if requirement and not requirement.startswith('#'):
+            requirements.append(requirement)
+
 setup(
     name="xent",
     version="0.1",
-    packages=find_packages(where=env_dir)
+    packages=find_packages(where=env_dir),
+    install_requires=requirements
 )
